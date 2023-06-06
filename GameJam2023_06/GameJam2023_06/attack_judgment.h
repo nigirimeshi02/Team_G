4fbf38a69534dca0e8023c8f4fd1844307179241
taiki0627 <104492163+taiki0627@users.noreply.github.com>
2023-06-06 11:10:56 +0900

    atck

---
+++ GameJam2023_06/GameJam2023_06/attack_judgment.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <stdio.h>
+#include "DxLib.h"
+#include ""
+
+class Attack_Judgment:
+{
+public:
+	Attack_Judgment();
+	~Attack_Judgment();
+
+private:
+
+};
+
+Attack_Judgment::Attack_Judgment()
+{
+}
+
+Attack_Judgment::~Attack_Judgment()
+{
+}
