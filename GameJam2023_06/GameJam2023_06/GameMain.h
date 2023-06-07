f82ae801d482ff25a50b286dd7fb274976dfd92f
taiki0627 <104492163+taiki0627@users.noreply.github.com>
2023-06-07 19:53:20 +0900

    G_main

--- GameJam2023_06/GameJam2023_06/GameMain.h
+++ GameJam2023_06/GameJam2023_06/GameMain.h
@@ -1,1 +1,15 @@
 #pragma once
+#pragma once
+#include "SceneManager/AbstractScene.h"
+
+class GameMain : public AbstractScene {
+public:
+	// デストラクタ
+	virtual ~GameMain() {};
+
+	// 描画以外の更新を実装する
+	virtual AbstractScene* Update() override;
+
+	// 描画に関することを実装する
+	virtual void Draw() const override;
+};
