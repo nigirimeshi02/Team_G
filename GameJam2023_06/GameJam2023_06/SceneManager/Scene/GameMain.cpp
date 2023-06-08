5c82bf43cbf0070196c90d95b0fc344fc65b199c
Syoki106 <122337008+Syoki106@users.noreply.github.com>
2023-06-08 10:48:02 +0900

    UI

--- GameJam2023_06/GameJam2023_06/SceneManager/Scene/GameMain.cpp
+++ GameJam2023_06/GameJam2023_06/SceneManager/Scene/GameMain.cpp
@@ -1,12 +1,25 @@
 #include "GameMain.h"
 
 #include "DxLib.h"
+
+int bananaImg;
+int EnemyImg;
+int heartImgs[3];
+
+// 体力の初期状態
+int heartMax = 3;
+int KeyPressed = FALSE;
+
 /*
 * コンストラクタ
 */
 GameMain::GameMain()
 {
-
+	bananaImg = LoadGraph("images/banana.png");
+	EnemyImg = LoadGraph("images/enemy.png");
+	heartImgs[0] = LoadGraph("images/heart.png");
+	heartImgs[1] = LoadGraph("images/heart.png");
+	heartImgs[2] = LoadGraph("images/heart.png");
 }
 
 /*
@@ -31,5 +44,39 @@ AbstractScene* GameMain::Update()
 */
 void GameMain::Draw()const
 {
+	DrawBox(0, 650, 1280, 720, 0xa0522d, TRUE);
+
+	// まんぷくゲージの表示
+	DrawExtendGraph(420, 660,470,710, bananaImg, TRUE);
+	DrawBox(490, 670, 750, 700, 0xffffff, TRUE);
+
+	// 敵の撃破数の表示
+	SetFontSize(40);
+	DrawExtendGraph(935, 655, 995, 715, EnemyImg, TRUE);
+	DrawFormatString(1000, 665, 0xffffff, "× 0");
+	//DrawFormatString(1000, 665, 0xffffff, "× %3d", gEnemyCount);
+
 	DrawFormatString(0, 0, 0xffffff, "test");
+
+	int SpaceKeyState = CheckHitKey(KEY_INPUT_SPACE);
+
+	// SPACEキーが押されると体力が減る
+	if (SpaceKeyState && !KeyPressed)
+	{
+		if (heartMax > 0)
+			heartMax--;
+		KeyPressed = TRUE;//キーが押されたフラグを立てる
+	}
+
+	// SPACEキーを離すとフラグをリセット
+	if (!SpaceKeyState && KeyPressed)
+	{
+		KeyPressed = FALSE;
+	}
+	
+	// 体力の表示
+	for (int i = 0; i < heartMax; i++)
+	{
+		DrawExtendGraph(110 + (i * 50), 660, 160 + (i * 50), 710, heartImgs[i], TRUE);
+	}
 }
